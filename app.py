4acdcefbebc7ae69d2ef8712150e3599c4d9368b
alexander-zhong <alexanderzousky@gmail.com>
2024-02-23 12:32:40 -0800

    Set up the general functions for the classifier

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 from flask import Flask, render_template
 
-import classifier
+import classifier 
+
+
+# Pre-Trains the model before running the flask app to prevent delays in user experience
+@app.before_first_request
+def before_first_request():
+    model = classifier.setup()
+    
+    
+    
--- classifier.py
+++ classifier.py
@@ -1,7 +1,19 @@
+import cv2
+import numpy
+import matplotlib.pyplot
+from keras import datasets, layers, models
 
 
 
 
+# Returns a trained model for classification
+def setup():
+    pass
 
 
+
+# REQUIRES: Must be a valid image and model
+# Function for the flask app to classify an image with a given model 
+def classify(model, img):
+    pass
 
