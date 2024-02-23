a8c10df02702e3204b88782a57306c3936c0d38d
Diego-Nisperos <diegonisperos1@gmail.com>
2024-02-23 14:24:38 -0800

    Added the Picture function for the user to upload photos. Edited the templates

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request 
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 import classifier 
 
 
-# Pre-Trains the model before running the flask app to prevent delays in user experience
-@app.before_first_request
-def before_first_request():
-    model = classifier.setup()
-    
 app = Flask(__name__)
+
+#Configures for our image classifier
+photo = UploadSet('photos', IMAGES)
+app.config['UPLOADED_PHOTOS_DEST'] = 'uploads'
+configure_uploads(app, photo)
+
 
 @app.route("/")
 def index():
@@ -20,4 +22,8 @@ def camera():
 
 @app.route("/picture")
 def picture():
+    if request.method == 'POST' and 'photo' in request.files:
+        filename = photo.save(request.files['photo'])
     return render_template("picture.html")
+
+
